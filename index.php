38b13db919e349f820feb9b5ca5b91ce2aa4bbbe
Abdelrahman Sayed <61325578+xBodda@users.noreply.github.com>
2021-06-01 22:34:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
+session_start();
+include("classes/Person.php");
+foreach (glob("classes/*.php") as $filename) {
+    if($filename != "classes/Person.php" && $filename != "classes/Order.php") {
+        include $filename;
+    }
+}
 
-// Creates a php Object
-$obj = new foo;
 
-// Also creates a php Object
-$obj = new foo();
+// include("classes/User.php");
+// include("classes/Admin.php");
+
+$user1 = new User("0","Abdelrahman","Sayed","email","01158999135","1010abab",0,"Egypt");
+
+$user1::makeOrder();
+
+
+?>
