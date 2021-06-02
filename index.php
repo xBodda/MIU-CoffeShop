4512f0285c2f892abb9492cdd3fd3291fefb5e10
Abdelrahman Sayed <61325578+xBodda@users.noreply.github.com>
2021-06-02 19:06:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,12 @@
 <?php
-session_start();
-include("classes/Person.php");
-foreach (glob("classes/*.php") as $filename) {
-    if($filename != "classes/Person.php" && $filename != "classes/Order.php") {
-        include $filename;
-    }
-}
+include("includes/head.php");
 
 
-// include("classes/User.php");
-// include("classes/Admin.php");
 
-$user1 = new User("0","Abdelrahman","Sayed","email","01158999135","1010abab",0,"Egypt");
+// User::Signup($firstname,"Sayed","email","01158999135","1010abab",0,"Egypt",0);
 
-$user1::makeOrder();
+
+
 
 
 ?>
