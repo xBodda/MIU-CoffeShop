df116e47788b8e2eb0fadde8621b17878fd76903
Abdelrahman Sayed <61325578+xBodda@users.noreply.github.com>
2021-06-04 21:47:35 +0200

    Update products.php

--- products.php
+++ products.php
@@ -12,23 +12,28 @@ foreach($all_drinks as $drink) {
 }
 
 
-
-
-
 // * To Make an Order
-$myOrder = array();
+$myOrder = $_SESSION['myOrder'];
 
-$Mochaa = new OrderItem("Mochaa","Skimmed Milk");
-array_push($myOrder,$Mochaa);
+// $Mochaa = new OrderItem("Mochaa","Skimmed Milk");
+// array_push($myOrder,$Mochaa);
 
-$Cappu = new OrderItem("Cappu","Extra Foam");
-array_push($myOrder,$Cappu);
+
 
 // $user->makeOrder($myOrder);
 if(isset($_GET['id'])) {
     $product = Drink::viewSpecficDrink($_GET['id']);
+    $all_beverages = Beverage::viewBeverages();
 } else {
     die("Not Found");
+}
+
+if(isset($_GET['id']) && isset($_GET['add'])) {
+    $orderID = $_GET['add'];
+    $orde = new OrderItem($orderID,"Extra Foam");
+    array_push($myOrder,$orde);
+
+    $_SESSION['myOrder'] = $myOrder;
 }
 
 ?>
@@ -51,11 +56,11 @@ if(isset($_GET['id'])) {
             <img src="layout/images/logo.jfif" alt="">
         </div>
         <div class="nav flex">
-            <div class="item">Home</div>
+            <a href="index.php"><div class="item">Home</div></a>
             <div class="item">Our Menu</div>
-            <div class="item">Order History</div>
-            <div class="item">Login</div>
-            <div class="item">Sign Up</div>
+            <a href="history.php"><div class="item">Order History</div></a>
+            <a href="signin.php"><div class="item">Login</div></a>
+            <a href="signup.php"><div class="item">Sign Up</div></a>
         </div>
     </div>
 
@@ -68,7 +73,7 @@ if(isset($_GET['id'])) {
             </div>
             <div class="product-info">
                 <div class="title"><?php echo $product['name'] ?></div>
-                <div class="price"><?php echo $product['price'] ?> L.E</div>
+                <div class="price" id="price"><?php echo $product['price'] ?> L.E</div>
                 <div class="overview">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, tempora. Labore harum impedit quas deleniti totam amet, eligendi, id est mollitia accusamus accusantium voluptas sequi porro laudantium pariatur. Id, provident.
                 </div>
@@ -76,29 +81,21 @@ if(isset($_GET['id'])) {
                     Extra Options
                 </h1>
                 <div class="order-items checkbox">
+                <?php 
+                    foreach($all_beverages as $beverage) {
+                ?>
                     <div class="item">
                         <div class="icon">
-                            <i class="fas fa-cookie-bite"></i>
+                            <?php echo $beverage['image'] ?>
                         </div>
-                        <p>Chocolate</p>
+                        <p><?php echo $beverage['name'] ?></p>
+                        <input type="hidden" value="<?php echo $beverage['price'] ?>" name="bevPrice" id="bevPrice">
                     </div>
-                    <div class="item ">
-                        <div class="icon">
-                            <i class="fas fa-blender"></i>
-                        </div>
-                        <p>Skimmed Milk</p>
-                    </div>
-                    <div class="item ">
-                        <div class="icon">
-                            <i class="fas fa-ring"></i>
-                        </div>
-                        <p>Soy</p>
-                    </div>
+                <?php } ?>
                 </div>
-                <div class="xbutton">Add to cart</div>
+                <a href="products.php?id=<?php echo $product['id']; ?>&add=<?php echo $product['id']; ?>"><div class="xbutton">Add to cart</div></a>
             </div>
         </div>
-        
 
     </div>
 </body>
